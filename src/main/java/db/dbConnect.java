8c449b67bb7d7e91eea813182cbab1e065f55b06
Guilherme Pinheiro <pinheiropgui@gmail.com>
2021-04-12 21:20:20 +0100

    add db

---
+++ src/main/java/db/dbConnect.java
@@ -0,0 +1,88 @@
+package db;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+import java.util.Arrays;
+
+/**
+ * Class dbConnect is used to: communicate with Heroku DataBase and create a Prepared Statements to send to the DB.
+ * Date: March-3-2021.
+ * @author Group A5_C.
+ */
+public class dbConnect {
+    /**
+     * Store Heroku URI: User + Password + Host + Port + Database.
+     * Private - Nobody needs to know what URI we are using.
+     * Static and Final - Never changes.
+     */
+    private static final String heroku_url="postgres://vkgvpttoqwwjti:241f4c5e49e1ff17e84ecab4bbe8c63ead0a80d1684405a3c5fa8542f36de5c0@ec2-54-74-35-87.eu-west-1.compute.amazonaws.com:5432/d2j57fljq86oa0";
+    /**
+     * Store DB Connection.
+     * Private - Nobody needs to know the db connection.
+     */
+    private Connection conn = null;
+
+    /**
+     * Constructor that creates connection with DataBase.
+     */
+    public dbConnect() {
+        try {
+            this.conn = getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to make first connection.
+     * @return DB Connection.
+     * @throws URISyntaxException if occurs an URI error in syntax.
+     * @throws SQLException if occurs an error in DB.
+     */
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(heroku_url);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    /**
+     * Method to get DB Connection.
+     * @return DB Connection.
+     */
+    public Connection getConn() {
+        return this.conn;
+    }
+
+    public void searchPath() throws SQLException {
+        PreparedStatement s = this.conn.prepareStatement("SET search_path TO ii;");
+        s.executeUpdate();
+    }
+
+    public Object[][] getTransform() throws SQLException {
+        PreparedStatement s = this.conn.prepareStatement("SELECT COUNT(*) FROM ii.\"Transform\";");
+        ResultSet rs = s.executeQuery();
+        rs.next();
+        Object[][] data = new Object[rs.getInt(1)][4];
+
+        s = this.conn.prepareStatement("SELECT \"OrderNumber\", \"from\", \"to\", quantity FROM ii.\"Transform\";");
+        rs = s.executeQuery();
+
+        int i=0;
+        while (rs.next()) {
+            data[i][0] = rs.getInt(1);
+            data[i][1] = rs.getInt(2);
+            data[i][2] = rs.getInt(3);
+            data[i][3] = rs.getInt(4);
+            i++;
+        }
+
+        return data;
+    }
+
+}
+
---
+++ src/main/java/ui/UI.java
@@ -0,0 +1,82 @@
+package ui;
+
+import db.dbConnect;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import java.sql.SQLException;
+
+public class UI {
+    private JPanel rootPanel;
+    private JTable showTable;
+    private JButton fecharButton;
+    private JComboBox ComboBox;
+    private JList list1;
+    private JScrollPane pane;
+    private JButton atualizarButton;
+    private JTable ShowTable1;
+    private dbConnect db;
+
+    public UI(){
+        this.db = new dbConnect();
+        createTable();
+        createComboBox();
+        try {
+            createShowTable1();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void createShowTable1() throws SQLException {
+        Object[][] data = db.getTransform();
+        ShowTable1.setModel(new DefaultTableModel(data,new String[]{"NºOrdem","Peça Inicial","Peça Final", "Quantidade"}));
+    }
+
+
+    private void createTable()
+        {
+            Object[][] data={
+                    {"Num de Peças"},
+                    {"Tempo"}
+
+
+            };
+        showTable.setModel(new DefaultTableModel(
+            data,
+            new String[] {"Pecas", "Maquina 1", "Maquina 2", "Maquina 3", "Maquina 4", "Maquina 5", "Maquina 6", "Maquina 7", "Maquina 8" }
+
+        ));
+            TableColumnModel columns =showTable.getColumnModel();
+            columns.getColumn(0).setMinWidth(100);
+            columns.getColumn(1).setMinWidth(50);
+            columns.getColumn(2).setMinWidth(50);
+            columns.getColumn(3).setMinWidth(50);
+            columns.getColumn(4).setMinWidth(50);
+            columns.getColumn(5).setMinWidth(50);
+            columns.getColumn(6).setMinWidth(50);
+            columns.getColumn(7).setMinWidth(50);
+            columns.getColumn(8).setMinWidth(50);
+            DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
+            centerRender.setHorizontalAlignment(JLabel.CENTER);
+            columns.getColumn(0).setCellRenderer(centerRender);
+            columns.getColumn(1).setCellRenderer(centerRender);
+            columns.getColumn(2).setCellRenderer(centerRender);
+            columns.getColumn(3).setCellRenderer(centerRender);
+            columns.getColumn(4).setCellRenderer(centerRender);
+            columns.getColumn(5).setCellRenderer(centerRender);
+            columns.getColumn(6).setCellRenderer(centerRender);
+            columns.getColumn(7).setCellRenderer(centerRender);
+            columns.getColumn(8).setCellRenderer(centerRender);
+
+    }
+    private void createComboBox(){
+        ComboBox.setModel(new DefaultComboBoxModel(new String[]{"tapete superior", "tapete inferior"}));
+    }
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
