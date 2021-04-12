274da6819b63f607917ef4e11407b1978cf336d9
Guilherme Pinheiro <pinheiropgui@gmail.com>
2021-04-12 21:23:51 +0100

    add db

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -35,43 +35,38 @@ public class UI {
         ShowTable1.setModel(new DefaultTableModel(data,new String[]{"NºOrdem","Peça Inicial","Peça Final", "Quantidade"}));
     }
 
+    private void createTable() {
+        Object[][] data={
+                {"Num de Peças"},
+                {"Tempo"}
+        };
 
-    private void createTable()
-        {
-            Object[][] data={
-                    {"Num de Peças"},
-                    {"Tempo"}
+        showTable.setModel(new DefaultTableModel(data, new String[] {"Peças", "Maquina 1", "Maquina 2", "Maquina 3", "Maquina 4", "Maquina 5", "Maquina 6", "Maquina 7", "Maquina 8" }));
 
-
-            };
-        showTable.setModel(new DefaultTableModel(
-            data,
-            new String[] {"Pecas", "Maquina 1", "Maquina 2", "Maquina 3", "Maquina 4", "Maquina 5", "Maquina 6", "Maquina 7", "Maquina 8" }
-
-        ));
-            TableColumnModel columns =showTable.getColumnModel();
-            columns.getColumn(0).setMinWidth(100);
-            columns.getColumn(1).setMinWidth(50);
-            columns.getColumn(2).setMinWidth(50);
-            columns.getColumn(3).setMinWidth(50);
-            columns.getColumn(4).setMinWidth(50);
-            columns.getColumn(5).setMinWidth(50);
-            columns.getColumn(6).setMinWidth(50);
-            columns.getColumn(7).setMinWidth(50);
-            columns.getColumn(8).setMinWidth(50);
-            DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
-            centerRender.setHorizontalAlignment(JLabel.CENTER);
-            columns.getColumn(0).setCellRenderer(centerRender);
-            columns.getColumn(1).setCellRenderer(centerRender);
-            columns.getColumn(2).setCellRenderer(centerRender);
-            columns.getColumn(3).setCellRenderer(centerRender);
-            columns.getColumn(4).setCellRenderer(centerRender);
-            columns.getColumn(5).setCellRenderer(centerRender);
-            columns.getColumn(6).setCellRenderer(centerRender);
-            columns.getColumn(7).setCellRenderer(centerRender);
-            columns.getColumn(8).setCellRenderer(centerRender);
+        TableColumnModel columns =showTable.getColumnModel();
+        columns.getColumn(0).setMinWidth(100);
+        columns.getColumn(1).setMinWidth(50);
+        columns.getColumn(2).setMinWidth(50);
+        columns.getColumn(3).setMinWidth(50);
+        columns.getColumn(4).setMinWidth(50);
+        columns.getColumn(5).setMinWidth(50);
+        columns.getColumn(6).setMinWidth(50);
+        columns.getColumn(7).setMinWidth(50);
+        columns.getColumn(8).setMinWidth(50);
+        DefaultTableCellRenderer centerRender = new DefaultTableCellRenderer();
+        centerRender.setHorizontalAlignment(JLabel.CENTER);
+        columns.getColumn(0).setCellRenderer(centerRender);
+        columns.getColumn(1).setCellRenderer(centerRender);
+        columns.getColumn(2).setCellRenderer(centerRender);
+        columns.getColumn(3).setCellRenderer(centerRender);
+        columns.getColumn(4).setCellRenderer(centerRender);
+        columns.getColumn(5).setCellRenderer(centerRender);
+        columns.getColumn(6).setCellRenderer(centerRender);
+        columns.getColumn(7).setCellRenderer(centerRender);
+        columns.getColumn(8).setCellRenderer(centerRender);
 
     }
+
     private void createComboBox(){
         ComboBox.setModel(new DefaultComboBoxModel(new String[]{"tapete superior", "tapete inferior"}));
     }
